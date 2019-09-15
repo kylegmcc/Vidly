7ab9d3c334c7e8566c9eec1642f4364ab9701774
Kyle McCarthy <km799012@ohio.edu>
2019-09-15 13:42:44 -0400

    Added customer and movie  API.

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Genre { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public int NumberInStock { get; set; }
+    }
+}
